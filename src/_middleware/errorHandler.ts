43164794c185a0e5bcb224bce53f2069bb004915
joshpadernal29 <josh.padernal29@gmail.com>
2026-03-29 02:12:24 +0800

    added error handler

---
+++ src/_middleware/errorHandler.ts
@@ -0,0 +1,24 @@
+// src/_middleware/erroHandler.ts
+import type { Request, Response, NextFunction } from "express";
+
+export function erroHandler(
+    err: Error | string,
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Response | void {
+    if (typeof err === 'string') {
+        // custom application error
+        const is404 = err.toLowerCase().endsWith('not found');
+        const statusCode = is404 ? 404 : 408;
+        return res.status(statusCode).json({ message: err });
+    }
+
+    if (err instanceof Error) {
+        // standard eror object
+        return res.status(500).json({ message: err.message });
+    }
+
+    // fallback message
+    return res.status(500).json({ message: 'Internal Server Error!' });
+}
